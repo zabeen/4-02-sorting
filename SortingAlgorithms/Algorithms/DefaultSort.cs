96ef9860c1b0667d0a7c0776d2a6033ca1164fd6
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-01-29 22:49:03 +0000

    Use .Sort

--- SortingAlgorithms/Algorithms/DefaultSort.cs
+++ SortingAlgorithms/Algorithms/DefaultSort.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SortingAlgorithms.Algorithms
 {
@@ -11,7 +8,9 @@ namespace SortingAlgorithms.Algorithms
         public virtual IEnumerable<int> SortItems(IEnumerable<int> itemsToSort)
         {
             // use the in-built sort
-            return itemsToSort.OrderBy(i => i).ToList();
+            var items = itemsToSort.ToList();
+            items.Sort();
+            return items;
         }
     }
 }
