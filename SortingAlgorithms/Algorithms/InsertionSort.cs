41252667d378dc7a3cc8e95770b36f6e48fb009f
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-01-31 10:38:43 +0000

    Improved insertion sort

--- SortingAlgorithms/Algorithms/InsertionSort.cs
+++ SortingAlgorithms/Algorithms/InsertionSort.cs
@@ -8,23 +8,26 @@ namespace SortingAlgorithms.Algorithms
         public IEnumerable<int> SortItems(IEnumerable<int> itemsToSort)
         {
             var unsorted = new Queue<int>(itemsToSort);
-            var sorted = new List<int> {unsorted.Dequeue()};
+            var sorted = new List<int> { unsorted.Dequeue() };
 
             while (unsorted.Count > 0)
             {
                 var currentItem = unsorted.Dequeue();
 
-                for (var i = 0; i < sorted.Count; i++)
+                for (var i = sorted.Count - 1; i > -1; i--)
                 {
-                    if (currentItem <= sorted[i])
+                    if (currentItem >= sorted[i])
                     {
-                        sorted.Insert(i, currentItem);
+                        if (i == sorted.Count - 1)
+                            sorted.Add(currentItem);
+                        else
+                            sorted.Insert(i+1, currentItem);
                         break;
                     }
 
-                    if (i == sorted.Count - 1)
+                    if (i == 0)
                     {
-                        sorted.Add(currentItem);
+                        sorted.Insert(0, currentItem);
                         break;
                     }
                 }
