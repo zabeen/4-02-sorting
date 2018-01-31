145541907cedf635332a8201c838dd1d652bc38b
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-01-31 18:27:05 +0000

    added negative ints

--- SortingAlgorithms/Tests/ItemsToSort.cs
+++ SortingAlgorithms/Tests/ItemsToSort.cs
@@ -8,7 +8,7 @@ namespace SortingAlgorithms.Tests
 {
     public class ItemsToSort
     {
-        public readonly List<int> UnorderedItems = new List<int>() { 1, 3, 2, 1, 4, 0, 4 };
-        public readonly List<int> OrderedItems = new List<int>() { 0, 1, 1, 2, 3, 4, 4 };
+        public readonly List<int> UnorderedItems = new List<int>() { 1, 3, -1, 2, 1, -1, 4, 0, -2, 4 };
+        public readonly List<int> OrderedItems = new List<int>() { -2, -1, -1, 0, 1, 1, 2, 3, 4, 4 };
     }
 }
